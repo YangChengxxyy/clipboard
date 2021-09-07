054463bb6c4c45dea3fc5a92fddd2efc34f9e24b
YangChengxxyy <853364106@qq.com>
2021-09-07 19:13:19 +0800

    init

---
+++ clipboard-system/src/main/kotlin/cn/yycccloud/system/ClipboardSystemApplication.kt
@@ -0,0 +1,11 @@
+package cn.yycccloud.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClipboardSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClipboardSystemApplication>(*args)
+}
---
+++ clipboard-system/src/test/kotlin/cn/yycccloud/system/ClipboardSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.yycccloud.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClipboardSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
